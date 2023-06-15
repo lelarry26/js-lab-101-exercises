f323134a889514528cecc97d8c2d007212a80013
Larry Le <larrygsu@gmail.com>
2023-06-15 01:50:11 -0400

    Updating with half of level 3 exercises

--- exercises/level3.js
+++ exercises/level3.js
@@ -1,21 +1,27 @@
 function lvl3exercise1 () {
   // Create strings "hello" and a "world", return the concatenation of the two
-
+let text1 = "hello"
+let text2 = "world"
+return text1.concat(text2);
 }
 
 function lvl3exercise2 () {
   // Create a "hello" and a 12, return the concatenation of the two
-
+  let text1 = "hello"
+  let text2 = 12
+  return text1.concat(text2);
 }
 
 function lvl3exercise3 () {
   // Create a variable that equals 12 and convert it to a string with concatenation. Return it.
-
+let number = 12
+return String.concat(number);
 }
 
 function lvl3exercise4 () {
   // Create a "hello world!" string. Return the length of the string
-
+let text = "hello world!"
+return length(text);
 }
 
 function lvl3exercise5 () {
