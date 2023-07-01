fd44749a46eeccb389efa4693e6e767e163eb724
Larry Le <larrygsu@gmail.com>
2023-06-30 21:23:03 -0400

    Updating with ALL exercises completed successfully!

--- exercises/level3.js
+++ exercises/level3.js
@@ -15,16 +15,17 @@ function lvl3exercise2 () {
 function lvl3exercise3 () {
   // Create a variable that equals 12 and convert it to a string with concatenation. Return it.
 let number = 12
-return String.concat(number);
+return String(number);
 }
 
 function lvl3exercise4 () {
   // Create a "hello world!" string. Return the length of the string
 let text = "hello world!"
-return length(text);
+return (text.length);
 }
 
 function lvl3exercise5 () {
   // Create a "hello world!" string. Return the index of the word "world".
-
+  let text = "hello world!"
+  return(text.indexOf("w"));
 }
---
+++ js Example 6-13/Thursexamp.js
@@ -0,0 +1,19 @@
+//Create an array that holds the number 1-100. Print out all numbers that are not divisible by 5 and are not.
+let numbers = [];
+
+// for(let i = 0; i < 100; i++){
+//     numbers.push(i + 1);
+//     if((numbers[i] % 5 !== 0) && (numbers[i] % 2 !== 0)){
+//         console.log(numbers[i]);
+//     }
+// }
+
+for(let i = 0; i < 100; i++){
+    numbers.push(i + 1);
+        if(numbers[i] % 5 !== 0)
+            numbers.splice(i:1) {
+        
+        
+                
+console.log(numbers[i]);
+}
